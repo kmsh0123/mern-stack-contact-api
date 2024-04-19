3d6cd4e7f3723add8ecb96de69d2375c4a8665b6
kmsh0123 <komauk1998@gmail.com>
2024-04-19 13:40:14 +0630

    pagination search

--- Controllers/ContactController.js
+++ Controllers/ContactController.js
@@ -2,9 +2,22 @@ import ContactModel from "../Models/ContactModel.js"
 
 export const getContact = async(req,res)=>{
     try {
-    const contact = await ContactModel.find({},{_id : 1,name : 1,email : 1,phone : 1,address : 1,image : 1,createdAt : 1,updatedAt : 1});
-    res.status(200).json(contact)
-    } catch (error) {
+    const {page} = req.query;
+    const search = req.query.search || ""
+    
+    const limit = 5;
+
+    const skip = (page - 1) * limit;
+    const contact = await ContactModel.find({name:{$regex:search,$options:"i"}},{_id : 1,name : 1,email : 1,phone : 1,address : 1,image : 1,createdAt : 1,updatedAt : 1})
+    .skip(skip)
+    .limit(limit);
+    const contactCount = await ContactModel.countDocuments({
+        name:{$regex:search,$options:"i"}
+    });
+    const totalPage = Math.ceil(contactCount/limit);
+    res.status(200).json({totalPage : totalPage, page : page,limit : limit,contact:contact})
+    } 
+    catch (error) {
     res.status(500).json({message : error.message});
     }
 }
